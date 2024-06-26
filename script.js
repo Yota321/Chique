5400102cac89397df29171b2825fad2e12e9a894
Yota321 <62050293+Yota321@users.noreply.github.com>
2024-06-27 04:58:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,3 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeButton = document.querySelector('.close-button');
     const prevButton = document.querySelector('.prev-button');
     const nextButton = document.querySelector('.next-button');
-    const galleryItems = document.querySelectorAll('.gallery-item img');
-    let currentIndex = 0;
-
-    function toggleMenu() {
-        menu.classList.toggle('show');
-        hamburger.textContent = menu.class
